e8f9c0e5339b9dfad0db671a810871841839a789
Ren-B-7 <127970608+Ren-B-7@users.noreply.github.com>
2026-03-19 12:08:19 +0200

    Added exports for xdg defaults

--- .custom/exports.sh
+++ .custom/exports.sh
@@ -11,10 +11,14 @@ export PATH="$MASON_INSTALLED_LSPS:$PATH"
 export PIPBIN="$HOME/.local/bin/"
 export PATH="$PIPBIN:$PATH"
 
+export XDG_CONFIG_HOME="/home/renier/.config/"
+export XDG_CACHE_HOME="/home/renier/.cache/"
+export XDG_LOCAL_HOME="/home/renier/.local/"
+
 # Clean and deduplicate PATH
 export PATH=$(echo "$PATH" | tr ':' '\n' | sed 's:/*$::' | awk '!x[$0]++' | paste -sd ':' -)
 
 if [ -z "$SSH_AUTH_SOCK" ]; then
-   eval "$(ssh-agent -s)" > /dev/null
+    eval "$(ssh-agent -s)" > /dev/null
 fi
 
