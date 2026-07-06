670aa48c61eb5ea4c7e5caac6046eebd4db78b75
Ren-B-7 <127970608+Ren-B-7@users.noreply.github.com>
2026-07-06 22:16:34 +0200

    Add ollama aliases

--- .custom/aliases_set.sh
+++ .custom/aliases_set.sh
@@ -47,3 +47,6 @@ fi
 
 alias tablet-on='kscreen-doctor output.Virtual-1.enable && kscreen-doctor output.Virtual-1.mode.1920x1080@60'
 alias tablet-off='kscreen-doctor output.Virtual-1.disable'
+
+alias ollama-serve="taskset -c 0-5 ollama serve"
+alias olr="ollama run"
